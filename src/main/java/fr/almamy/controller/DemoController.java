040e0872acea5f7c0fd801e50310c4852c593691
Alm4my <thenixadm@gmail.com>
2021-06-27 22:58:07 +0000

    Make the controller return the string hello instead of the view
    @ResponseBody means that a method return value should be bound to the web response body. Here the string "Hello" will be written to the response body which the browser will display

--- src/main/java/fr/almamy/controller/DemoController.java
+++ src/main/java/fr/almamy/controller/DemoController.java
@@ -2,12 +2,14 @@ package fr.almamy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 
     // http://localhost:8080/todo-list/hello
     @GetMapping("/hello")
+    @ResponseBody
     public String Hello(){
         return "Hello";
     }
