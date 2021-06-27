94537f46823dc1f198b9dacb96334ccaaa47e1f2
Alm4my <thenixadm@gmail.com>
2021-06-27 22:31:17 +0000

    Create the Dispatcher Servlet and register it with the Servlet context (parameter of onStartup()). Tomcat was changed to 9x.
    We need to name our servlet before registering it (DISPATCHER_SERVLET_NAME).
    
    We use the servletContext.addServlet() which returns a ServletRegistration Dynamic which we use to configure the servlet (L:30-34)
    
    registration.setLoadOnStartup(1) enables us to configure the startup loading of the servlet so that the container will instantiate and initialize our servlet upon startup.
    
    registration.addMapping("/") maps our servlet to the URL pattern specified which here is "/". This will override the default tomcat homepage. We do this because we do not want to display the tomcat homepage to our users.

--- src/main/java/fr/almamy/config/WebAppInitializer.java
+++ src/main/java/fr/almamy/config/WebAppInitializer.java
@@ -3,12 +3,15 @@ package fr.almamy.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @Slf4j
 public class WebAppInitializer implements WebApplicationInitializer {
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
@@ -17,7 +20,19 @@ public class WebAppInitializer implements WebApplicationInitializer {
         // Create the spring application context
         var context = new AnnotationConfigWebApplicationContext();
 
-        // registering a configuration class (our WebConfig class in this instance)
+        // Registering a configuration class (our WebConfig class in this instance)
         context.register(WebConfig.class);
+
+        // Create the dispatcher servlet
+        var dispatcherServlet = new DispatcherServlet(context);
+
+        // Register and configure the dispatcher servlet
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+
+
     }
 }
