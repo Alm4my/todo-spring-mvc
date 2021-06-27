a448f878703595f9a0ab165384965261154034ff
Alm4my <thenixadm@gmail.com>
2021-06-27 22:04:10 +0000

    Create application context (AnnotationConfigWebApplicationContext) and register our configuration class
    onStartup() is used to configure the web application; that is, initialize it at startup.
    Note that the servletContext (WebAppInitializer L:13) isn't the same as the Spring ApplicationContext. The servletContext defines a set of methods which a servlet uses to communicate with its servlet container (Tomcat in this case)
    
    For a configuration class to be registered, it needs to have the @Configuration annotation.

--- src/main/java/fr/almamy/config/WebAppInitializer.java
+++ src/main/java/fr/almamy/config/WebAppInitializer.java
@@ -1,13 +1,23 @@
 package fr.almamy.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+@Slf4j
 public class WebAppInitializer implements WebApplicationInitializer {
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("onStartup");
 
+        // Create the spring application context
+        var context = new AnnotationConfigWebApplicationContext();
+
+        // registering a configuration class (our WebConfig class in this instance)
+        context.register(WebConfig.class);
     }
 }
