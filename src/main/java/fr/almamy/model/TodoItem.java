dc9a214143adc3271359ef27c7e8bce8f82dfa10
Alm4my <thenixadm@gmail.com>
2021-06-30 14:29:42 +0000

    Create TodoItem class to represent a single ToDo item
    With the @Data annotation, getter, setters, equals, hashCode, and some other methods will be automatically created by lombok for us with default value. We can view them in the structure tab.
    
    We also can fine tune this generation. For instance, for the equals and hashcode, we wanted them only for the id field, so we used @EqualsAndHashCode(of = "id") we use the name of teh field as the string value. If we want more than one, we can add an array of string as @EqualsAndHashCode(of = {"id", "title"})

---
+++ src/main/java/fr/almamy/model/TodoItem.java
@@ -0,0 +1,25 @@
+package fr.almamy.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(of = "id")
+public class TodoItem {
+
+    // == Fields ==
+    private int id;
+    private String title;
+    private String details;
+    private LocalDateTime deadline;
+
+    // == Constructor ==
+    public TodoItem(int id, String title, String details, LocalDateTime deadline) {
+        this.id = id;
+        this.title = title;
+        this.details = details;
+        this.deadline = deadline;
+    }
+}
