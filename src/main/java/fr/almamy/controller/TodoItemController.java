f73f01da2d8fcdc876bbd54ecd2e59d35ba0f009
Alm4my <thenixadm@gmail.com>
2021-07-01 17:48:04 +0000

    Log post data and create addEdit to display the form
    Since we bound the attributes in the view, when displaying it (before post with get), we need to give dummy values as with the following two line. Else, Tomcat will throw an error.
    
            var todoItem = new TodoItem("", "", LocalDateTime.now());
            model.addAttribute(AttributeNames.TODO_ITEM, todoItem);

--- src/main/java/fr/almamy/controller/TodoItemController.java
+++ src/main/java/fr/almamy/controller/TodoItemController.java
@@ -6,13 +6,18 @@ import fr.almamy.service.TodoItemService;
 import fr.almamy.util.AttributeNames;
 import fr.almamy.util.Mappings;
 import fr.almamy.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
+@Slf4j
 public class TodoItemController {
     // == Fields ==
     private final TodoItemService todoItemService;
@@ -37,8 +42,17 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEdit(Model model){
+        var todoItem = new TodoItem("", "", LocalDateTime.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem){
+        log.info("todoItem from form = {}", todoItem);
+        todoItemService.addItem(todoItem);
         return "redirect:/" + Mappings.ITEMS;
     }
 }
--- src/main/java/fr/almamy/util/ViewNames.java
+++ src/main/java/fr/almamy/util/ViewNames.java
@@ -3,6 +3,7 @@ package fr.almamy.util;
 public class ViewNames {
     // == Constants ==
     public static final String ITEMS_LIST = "items_list";
+    public static final String ADD_ITEM = "add_item";
 
     // == Constructors ==
     private ViewNames() {}
